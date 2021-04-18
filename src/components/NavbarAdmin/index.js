062170717f5fb8e917241a7c42f2f63364a7403f
BrandonTuan777 <you@example.com>
2021-04-18 08:30:54 +0700

    fix

--- src/components/NavbarAdmin/index.js
+++ src/components/NavbarAdmin/index.js
@@ -1,47 +1,93 @@
-import React, { Component } from "react";
-import { NavLink } from "react-router-dom";
+import React, { Component,useState } from "react";
 
-export default class index extends Component {
-  render() {
+// import clsx from "clsx";
+// import { makeStyles } from "@material-ui/core/styles";
+// import CssBaseline from "@material-ui/core/CssBaseline";
+// import Drawer from "@material-ui/core/Drawer";
+// import Box from "@material-ui/core/Box";
+// import AppBar from "@material-ui/core/AppBar";
+// import Toolbar from "@material-ui/core/Toolbar";
+// import List from "@material-ui/core/List";
+// import Typography from "@material-ui/core/Typography";
+// import Divider from "@material-ui/core/Divider";
+// import IconButton from "@material-ui/core/IconButton";
+// import Badge from "@material-ui/core/Badge";
+// import Container from "@material-ui/core/Container";
+// import Grid from "@material-ui/core/Grid";
+// import Paper from "@material-ui/core/Paper";
+
+// import MenuIcon from "@material-ui/icons/Menu";
+// import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+// import NotificationsIcon from "@material-ui/icons/Notifications";
+// import ListItem from "@material-ui/core/ListItem";
+// import ListItemIcon from "@material-ui/core/ListItemIcon";
+// import ListItemText from "@material-ui/core/ListItemText";
+// import ListSubheader from "@material-ui/core/ListSubheader";
+// import DashboardIcon from "@material-ui/icons/Dashboard";
+// import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+// import PeopleIcon from "@material-ui/icons/People";
+// import BarChartIcon from "@material-ui/icons/BarChart";
+// import LayersIcon from "@material-ui/icons/Layers";
+// import AssignmentIcon from "@material-ui/icons/Assignment";
+// import SingIn from "../../HomeTemplate/SingIn";
+// import AddUserPage from "../AddUserPage";
+// import Footer from "../../../components/Footer";
+//
+
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+import { Link } from 'react-router-dom';
+import { SidebarData } from '../../container/AdminTemplate/SidebarData';
+import '../../container/AdminTemplate/Navbar.css';
+import { IconContext } from 'react-icons';
+
+// import { mainListItems, secondaryListItems } from './listItems';
+
+
+
+
+function NavbarAdmin()  {
+
+  
+  const [sidebar, setSidebar] = useState(false);
+
+  const showSidebar = () => setSidebar(!sidebar);
+  var obj = JSON.parse(localStorage.getItem("UserAdmin"))
     return (
-      <nav className="navbar navbar-expand-md bg-dark navbar-dark">
-        {/* Brand */}
-        <a className="navbar-brand" href="#a">
-          Navbar
-        </a>
-        {/* Toggler/collapsibe Button */}
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#collapsibleNavbar"
-        >
-          <span className="navbar-toggler-icon" />
-        </button>
-        {/* Navbar links */}
-        <div className="collapse navbar-collapse" id="collapsibleNavbar">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <NavLink
-                activeClassName="active"
-                className="nav-link"
-                to="/dashboard"
-              >
-                Dashborad
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink
-                activeClassName="active"
-                className="nav-link"
-                to="/add-user"
-              >
-                AddUser
-              </NavLink>
-            </li>
-          </ul>
+      <>
+      <IconContext.Provider value={{ color: '#fff' }}>
+        <div className='navbar'>
+          <Link to='#' className='menu-bars'>
+            <FaIcons.FaBars onClick={showSidebar} />
+          </Link>
+          <h1 style={{justifyContent:"flex-end",color:"white"}}>
+          {obj.hoTen}
+         
+          </h1>
         </div>
-      </nav>
-    );
+        <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+          <ul className='nav-menu-items' onClick={showSidebar}>
+            <li className='navbar-toggle'>
+              <Link to='#' className='menu-bars'>
+                <AiIcons.AiOutlineClose />
+              </Link>
+            </li>
+            {SidebarData.map((item, index) => {
+              return (
+                <li key={index} className={item.cName}>
+                  <Link to={item.path}>
+                    {item.icon}
+                    <span>{item.title}</span>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </IconContext.Provider>
+    </>    );
   }
-}
+
+
+
+export default NavbarAdmin;
