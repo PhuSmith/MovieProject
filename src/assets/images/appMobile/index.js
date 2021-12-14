4e45938119406b9b3813f23e661239f7da46f720
Phu <duongquocphu1996@gmail.com>
2021-12-14 14:53:28 +0700

    add images

---
+++ src/assets/images/appMobile/index.js
@@ -0,0 +1,19 @@
+import mobile from "./mobile.png";
+import slide1 from "./slide1.jpg";
+import slide2 from "./slide2.jpg";
+import slide3 from "./slide3.jpg";
+import slide4 from "./slide4.jpg";
+import slide5 from "./slide5.jpg";
+import slide7 from "./slide7.jpg";
+import slide8 from "./slide8.jpg";
+const images = {
+  mobile,
+  slide1,
+  slide2,
+  slide3,
+  slide4,
+  slide5,
+  slide7,
+  slide8,
+};
+export default images;
---
+++ src/assets/images/footer/index.js
@@ -0,0 +1,53 @@
+import cgv from "./cgv.png";
+import bhd from "./bhd.png";
+import galaxy from "./galaxycine.png";
+import star from "./cinestar.png";
+import go123 from "./123go.png";
+import agribank from "./AGRIBANK.png";
+import megags from "./megags.png";
+import beta from "./bt.jpg";
+import dongda from "./dongdacinema.png";
+import touch from "./TOUCH.png";
+import cnx from "./cnx.jpg";
+import STARLIGHT from "./STARLIGHT.png";
+import dcine from "./dcine.png";
+import zalopay_icon from "./zalopay_icon.png";
+import payoo from "./payoo.jpg";
+import VCB from "./VCB.png";
+import VIETTINBANK from "./VIETTINBANK.png";
+import IVB from "./IVB.png";
+import laban from "./laban.png";
+import appleLogo from "./apple-logo.png";
+import androidLogo from "./android-logo.png";
+import facebookLogo from "./facebook-logo.png";
+import zaloLogo from "./zalo-logo.png";
+import zionLogo from "./zion-logo.jpg";
+import bocongthuong from "./bocongthuong.png";
+const images = {
+  cgv,
+  bhd,
+  galaxy,
+  star,
+  go123,
+  agribank,
+  megags,
+  beta,
+  dongda,
+  touch,
+  cnx,
+  STARLIGHT,
+  dcine,
+  zalopay_icon,
+  payoo,
+  VCB,
+  VIETTINBANK,
+  IVB,
+  laban,
+  appleLogo,
+  androidLogo,
+  facebookLogo,
+  zaloLogo,
+  zionLogo,
+  bocongthuong,
+};
+export default images;
